c077dc5064123567bc3312314c49fa71db10772e
ukn <122321262+ukn-repos@users.noreply.github.com>
2023-01-10 12:39:09 -0500

    Update Relay.lua

--- Relay.lua
+++ Relay.lua
@@ -1,1 +1,96 @@
-lol
+menu.divider(menu.my_root(), "Downloading assets...")
+
+local dataset = {
+	"2Take1-Footer",
+	"2Take1-Header",
+	"Circuit-Footer",
+	"Circuit-Header",
+	"Epsilon-Footer",
+	"Epsilon-Overlay",
+	"Epsilon-Subheader",
+	"Fragment-Footer",
+	"Fragment-Header",
+	"Fragment-Subheader",
+	"Impulse-Footer",
+	"Luna-Footer",
+	"Luna-Header",
+	"Luna-Subheader",
+	"Ozark-Overlay",
+	"Ozark-Subheader",
+	"Paragon-Footer",
+	"Paragon-Header",
+	"Paragon-Subheader",
+	"Phantom-X-Footer",
+	"Phantom-X-Header",
+	"Phantom-X-Subheader",
+	"Rebound-Footer",
+	"Rebound-Header",
+	"Rebound-Subheader",
+	"Serendipity-Header",
+	"Terror-Footer",
+	"Terror-Header",
+	"Terror-Subheader",
+	"The Purge-Header",
+	"UniversalScrollbar",
+	"X-Force-Footer",
+	"X-Force-Header",
+	"X-Force-Subheader",
+	"XCheats-Footer",
+	"XCheats-Header",
+	"XCheats-Subheader"
+}
+
+filesystem.mkdir(filesystem.scripts_dir().."RTL/")
+util.toast("Created directory")
+
+for i = 1, 37 do
+	local Completed = false
+
+	util.toast(i)
+	async_http.init("raw.githubusercontent.com", "ukn-repos/rtl/main/assembler/" .. dataset[i] .. ".bmp", function(createfile)
+    	file = io.open(filesystem.scripts_dir() .. "RTL/" .. dataset[i] .. ".bmp", "wb")
+    	file:write(createfile) file:close() 
+		util.toast("Imported " .. dataset[i] .. " !")
+		Completed = true
+	end) 
+	async_http.dispatch()
+
+	repeat util.yield() until Completed
+end
+
+for i = 1, 50 do --impulse
+	local Completed = false
+
+	async_http.init("raw.githubusercontent.com", "ukn-repos/rtl/main/assembler/Impulse-Header" .. i .. ".bmp", function(createfile)
+    	file = io.open(filesystem.scripts_dir() .. "RTL/Impulse-Header" .. i .. ".bmp", "wb")
+    	file:write(createfile) file:close() 
+		util.toast("Imported Impulse Header " .. i .. " !")
+		Completed = true
+	end) 
+	async_http.dispatch()
+
+	repeat util.yield() until Completed
+end
+
+for i = 1, 18 do --interaction
+	local Completed = false
+
+	util.toast(i)
+	async_http.init("raw.githubusercontent.com", "ukn-repos/rtl/main/assembler/Interaction-Header" .. i .. ".bmp", function(createfile)
+    	file = io.open(filesystem.scripts_dir() .. "RTL/Interaction-Header" .. i .. ".bmp", "wb")
+    	file:write(createfile) file:close() 
+		util.toast("Imported Interaction Header " .. i .. " !")
+		Completed = true
+	end) 
+	async_http.dispatch()
+
+	repeat util.yield() until Completed
+end
+
+async_http.init("raw.githubusercontent.com", "ukn-repos/rtl/main/RTL.lua", function(update)
+    source = io.open(filesystem.scripts_dir() .. SCRIPT_RELPATH, "wb")
+    source:write(update) 
+	source:close() 
+end) 
+async_http.dispatch() 
+util.restart_script()
