0106776f0c3b9b956149f54a741c9120d41d6dbc
nealcaffrey259 <119753157+nealcaffrey259@users.noreply.github.com>
2023-01-26 21:47:36 -0800

    fix: actually prevent useless draw calls

--- RTL.lua
+++ RTL.lua
@@ -1901,16 +1901,13 @@ function use_theme(name)
     menu.trigger_commands("addressbar hide; " .. (name == "Stand" and "addressbar;" or "") .. "clearstandnotifys;") 
     menu.trigger_command(menu.ref_by_path("Stand>Lua Scripts>RTL"))
 
-    -- the rest dont need to run on default theme
-    if theme.name == "Stand" then
-        return
-    end
-
     if dx.header.animated then
         local dir = if dx.header.interaction_header then "\\.interaction" else "\\" .. theme.name
         directx.create_texture(script_dir .. dir .. "\\Header1.bmp")
     else
-        headerDX = directx.create_texture(script_dir .. "\\" .. theme.dirname .. "\\Header.bmp")
+        if theme.name ~= "Stand" then 
+            headerDX = directx.create_texture(script_dir .. "\\" .. theme.dirname .. "\\Header.bmp")
+        end
     end
 
     if dx.overlay.active then
